9ca905046db6f4d2a01f68cb677a021804c8fc77
KIM JUNSUNG <naber9065@naver.com>
2019-05-24 23:38:01 +0900

    회원 정보를 수정하기 위한 SQL작성과 메소드 작성 및 JUnit 단위 테스트 완료

--- overtheflow/src/main/java/com/studyveloper/overtheflow/mapper/MemberMapper.java
+++ overtheflow/src/main/java/com/studyveloper/overtheflow/mapper/MemberMapper.java
@@ -5,4 +5,5 @@ import com.studyveloper.overtheflow.vo.MemberVO;
 public interface MemberMapper {
 	public void registerMember(MemberVO memberVO);
 	public int unregisterMember(MemberVO memberVO);
+	public int modifyMember(MemberVO memberVO);
 }
