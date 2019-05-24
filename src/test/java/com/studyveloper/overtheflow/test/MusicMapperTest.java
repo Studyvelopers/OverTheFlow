de5695df1b89bc7d6ce4a005becc578fc17acba5
JinGuLee <dlwlsrn94@naver.com>
2019-05-24 21:55:39 +0900

    MusicMapper 테스트용 JUnit Test 추가

---
+++ src/test/java/com/studyveloper/overtheflow/test/MusicMapperTest.java
@@ -0,0 +1,29 @@
+package com.studyveloper.overtheflow.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:musicmapper-context.xml"})
+public class MusicMapperTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
