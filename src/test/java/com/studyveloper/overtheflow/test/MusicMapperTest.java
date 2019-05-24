dde0ce53ddedc19a6e0353ad210fb976041f7fdd
JinGuLee <dlwlsrn94@naver.com>
2019-05-25 08:37:54 +0900

    MusicMapper addMusisc메소드 테스트 완료

--- src/test/java/com/studyveloper/overtheflow/test/MusicMapperTest.java
+++ src/test/java/com/studyveloper/overtheflow/test/MusicMapperTest.java
@@ -1,18 +1,22 @@
 package com.studyveloper.overtheflow.test;
 
-import static org.junit.Assert.fail;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studyveloper.overtheflow.mapper.MusicMapper;
+import com.studyveloper.overtheflow.vo.MusicVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:musicmapper-context.xml"})
 public class MusicMapperTest {
-
+	@Autowired
+	private MusicMapper musicMapper;
+	
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -22,8 +26,18 @@ public class MusicMapperTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addMusicTest() {
+		MusicVO musicVO = new MusicVO();
+		String id = Integer.toString(musicVO.hashCode());
+		musicVO.setId(id);
+		musicVO.setTitle("테스트용 타이틀1");
+		musicVO.setPlayTime(0);
+		musicVO.setVisibilityFlag(true);
+		musicVO.setDownloadFlag(false);
+		musicVO.setDescription("");
+		musicVO.setCategoryId("0");
+		musicVO.setMemberId("0");
+		this.musicMapper.addMusic(musicVO);
 	}
 
 }
