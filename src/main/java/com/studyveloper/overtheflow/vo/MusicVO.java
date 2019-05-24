88211b88db441750955b9d95474c54c19c511596
JinGuLee <dlwlsrn94@naver.com>
2019-05-25 08:34:22 +0900

    MusicVO 해시코드 추가

--- src/main/java/com/studyveloper/overtheflow/vo/MusicVO.java
+++ src/main/java/com/studyveloper/overtheflow/vo/MusicVO.java
@@ -88,6 +88,23 @@ public class MusicVO {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categoryId == null) ? 0 : categoryId.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (downloadFlag ? 1231 : 1237);
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		result = prime * result + playCount;
+		result = prime * result + playTime;
+		result = prime * result + ((registerDate == null) ? 0 : registerDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + (visibilityFlag ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
