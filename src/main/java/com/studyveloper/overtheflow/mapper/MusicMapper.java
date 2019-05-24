941349cf5e27e0f31b020e3c3c8ac12282960406
JinGuLee <dlwlsrn94@naver.com>
2019-05-24 20:29:10 +0900

    MusicMapper 인터페이스 추가

---
+++ src/main/java/com/studyveloper/overtheflow/mapper/MusicMapper.java
@@ -0,0 +1,10 @@
+package com.studyveloper.overtheflow.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.studyveloper.overtheflow.vo.MusicVO;
+
+public interface MusicMapper {
+	
+}
