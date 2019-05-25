dc0a10edd7e5b701cc215e63723a5c1372f9fd0a
JinGuLee <dlwlsrn94@naver.com>
2019-05-25 21:33:44 +0900

    MemberLikesMusicMapper add 메소드, 쿼리 추가

---
+++ src/main/java/com/studyveloper/overtheflow/mapper/MemberLikesMusicMapper.java
@@ -0,0 +1,7 @@
+package com.studyveloper.overtheflow.mapper;
+
+import com.studyveloper.overtheflow.vo.MemberLikesMusicVO;
+
+public interface MemberLikesMusicMapper {
+	public int addMemberLikesMusic(MemberLikesMusicVO memberLikesMusicVO);
+}
---
+++ src/test/java/com/studyveloper/overtheflow/test/MemberLikesMusicMapperTest.java
@@ -0,0 +1,45 @@
+package com.studyveloper.overtheflow.test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.studyveloper.overtheflow.mapper.MemberLikesMusicMapper;
+import com.studyveloper.overtheflow.vo.MemberLikesMusicVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:memberlikesmusic-context.xml"})
+
+public class MemberLikesMusicMapperTest {
+	@Autowired
+	private MemberLikesMusicMapper memberLikesMusicMapper;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void addMemberLikesMusicTest() {
+		MemberLikesMusicVO memberLikesMusicVO = new MemberLikesMusicVO();
+		
+		memberLikesMusicVO.setMemberId("1");
+		memberLikesMusicVO.setMusicId("1");
+		
+		int result = this.memberLikesMusicMapper.addMemberLikesMusic(memberLikesMusicVO);
+		
+		assertThat(result, is(1));
+	}
+
+}
