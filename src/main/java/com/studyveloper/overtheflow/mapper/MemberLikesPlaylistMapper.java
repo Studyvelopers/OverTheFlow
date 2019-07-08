92fb011aceeca6e9614090e386f95be1888e11a0
Donghoon <dhjeon1993@gmail.com>
2019-07-08 19:09:33 +0900

    [추가] MemberLikesPlaylistMapper 좋아요하기 기능 추가

---
+++ src/main/java/com/studyveloper/overtheflow/mapper/MemberLikesPlaylistMapper.java
@@ -0,0 +1,7 @@
+package com.studyveloper.overtheflow.mapper;
+
+import com.studyveloper.overtheflow.vo.MemberLikesPlaylistVO;
+
+public interface MemberLikesPlaylistMapper {
+	public void likesPlaylist(MemberLikesPlaylistVO memberLikesPlaylistVO) throws Exception;
+}
