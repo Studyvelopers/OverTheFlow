4226dd85a330d5cbec32c9f6330e75e1489e8190
Dong-Hoon Jeon <dhjeon1993@gmail.com>
2019-07-08 22:00:05 +0900

    [수정] CategoryMapper 파일의 기초자료형을 래퍼클래스로 변경

--- src/main/java/com/studyveloper/overtheflow/mapper/CategoryMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/CategoryMapper.java
@@ -3,9 +3,9 @@ package com.studyveloper.overtheflow.mapper;
 import com.studyveloper.overtheflow.vo.CategoryVO;
 
 public interface CategoryMapper {
-	public int addCategory(CategoryVO categoryVO);
-	public int modifyCategory(CategoryVO categoryVO);
+	public Integer addCategory(CategoryVO categoryVO);
+	public Integer modifyCategory(CategoryVO categoryVO);
 	public void deleteAllCategory();
-	public boolean deleteCategory(String id);
+	public Boolean deleteCategory(String id);
 	public CategoryVO searchCategoryByCategoryId(String id);
 }
