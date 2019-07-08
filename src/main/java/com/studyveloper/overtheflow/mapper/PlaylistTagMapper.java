d28602718d1658eb8b62cb26019cd1c656765ba5
Donghoon <dhjeon1993@gmail.com>
2019-07-08 17:35:04 +0900

    [추가] PlaylistTagMapper 플레이리스트 식별키로 태그 삭제 기능 추가

--- src/main/java/com/studyveloper/overtheflow/mapper/PlaylistTagMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/PlaylistTagMapper.java
@@ -3,5 +3,7 @@ package com.studyveloper.overtheflow.mapper;
 import com.studyveloper.overtheflow.vo.PlaylistTagVO;
 
 public interface PlaylistTagMapper {
-	public void addPlaylistTag(PlaylistTagVO playlistTagVO);
+	public void addPlaylistTag(PlaylistTagVO playlistTagVO) throws Exception;
+	
+	public void deletePlaylistTagByPlaylistId(String playlistId) throws Exception;
 }
