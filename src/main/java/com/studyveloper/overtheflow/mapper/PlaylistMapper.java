08463a23f5f28552ef864eb5bbb951253e85bed2
Donghoon <dhjeon1993@gmail.com>
2019-07-08 15:51:17 +0900

    [추가] playlistMapper 회원식별키로 플레이리스트 목록 검색기능 추가

--- src/main/java/com/studyveloper/overtheflow/mapper/PlaylistMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/PlaylistMapper.java
@@ -1,9 +1,13 @@
 package com.studyveloper.overtheflow.mapper;
 
+import java.util.List;
+
 import com.studyveloper.overtheflow.vo.PlaylistVO;
 
 public interface PlaylistMapper {
 	public void addPlaylist(PlaylistVO playlistVO) throws Exception;
 	
 	public PlaylistVO searchPlaylistById(String id) throws Exception;
+	
+	public List<PlaylistVO> searchPlaylistsByMemberId(String memberId) throws Exception;
 }
