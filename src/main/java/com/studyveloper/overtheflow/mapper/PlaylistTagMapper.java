dd5d51855c45885dd998d8cc05325a12c6496596
Donghoon <dhjeon1993@gmail.com>
2019-07-08 17:41:26 +0900

    [추가] PlaylistTagMapper 한 플레이리스트에 태그 하나를 삭제하는 기능 추가

--- src/main/java/com/studyveloper/overtheflow/mapper/PlaylistTagMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/PlaylistTagMapper.java
@@ -6,4 +6,6 @@ public interface PlaylistTagMapper {
 	public void addPlaylistTag(PlaylistTagVO playlistTagVO) throws Exception;
 	
 	public void deletePlaylistTagsByPlaylistId(String playlistId) throws Exception;
+	
+	public void deletePlaylistTag(PlaylistTagVO playlistTagVO) throws Exception;
 }
