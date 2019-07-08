94e721b775f8213bf389c6cfa8992279238bad85
Donghoon <dhjeon1993@gmail.com>
2019-07-08 17:36:23 +0900

    [수정] PlaylistTagMapper 식별키로 태그 삭제하는 메소드명 변경

--- src/main/java/com/studyveloper/overtheflow/mapper/PlaylistTagMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/PlaylistTagMapper.java
@@ -5,5 +5,5 @@ import com.studyveloper.overtheflow.vo.PlaylistTagVO;
 public interface PlaylistTagMapper {
 	public void addPlaylistTag(PlaylistTagVO playlistTagVO) throws Exception;
 	
-	public void deletePlaylistTagByPlaylistId(String playlistId) throws Exception;
+	public void deletePlaylistTagsByPlaylistId(String playlistId) throws Exception;
 }
