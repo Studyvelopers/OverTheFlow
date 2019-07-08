60c9422e2dd8e72382d6f5f802e2ffcfa920cbb9
Donghoon <dhjeon1993@gmail.com>
2019-07-08 19:13:11 +0900

    [추가] MemberLikesPlaylistMapper 좋아요 취소 기능 추가

--- src/main/java/com/studyveloper/overtheflow/mapper/MemberLikesPlaylistMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/MemberLikesPlaylistMapper.java
@@ -4,4 +4,6 @@ import com.studyveloper.overtheflow.vo.MemberLikesPlaylistVO;
 
 public interface MemberLikesPlaylistMapper {
 	public void likesPlaylist(MemberLikesPlaylistVO memberLikesPlaylistVO) throws Exception;
+	
+	public void noLikesPlaylist(MemberLikesPlaylistVO memberLikesPlaylistVO) throws Exception;
 }
