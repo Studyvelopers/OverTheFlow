18cb521ec3f700e48a40fdc5d9a6430bea2611eb
Donghoon <dhjeon1993@gmail.com>
2019-07-08 19:21:48 +0900

    [추가] MemberLikesPlaylistMapper 회원 식별키로 좋아요한 플레이리스트 검색 기능 추가

--- src/main/java/com/studyveloper/overtheflow/mapper/MemberLikesPlaylistMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/MemberLikesPlaylistMapper.java
@@ -1,9 +1,14 @@
 package com.studyveloper.overtheflow.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import com.studyveloper.overtheflow.vo.MemberLikesPlaylistVO;
 
 public interface MemberLikesPlaylistMapper {
 	public void likesPlaylist(MemberLikesPlaylistVO memberLikesPlaylistVO) throws Exception;
 	
 	public void noLikesPlaylist(MemberLikesPlaylistVO memberLikesPlaylistVO) throws Exception;
+	
+	public List<String> searchPlaylistsByMemberId(Map<String, Object> conditions) throws Exception;
 }
