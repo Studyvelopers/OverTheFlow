da6f98f0d5314a908153e72a99c300ed80f31410
Dong-Hoon Jeon <dhjeon1993@gmail.com>
2019-07-09 09:04:01 +0900

    [수정] MusicTagMapper 기초자료형을 래퍼클래스로 변경

--- src/main/java/com/studyveloper/overtheflow/mapper/MusicTagMapper.java
+++ src/main/java/com/studyveloper/overtheflow/mapper/MusicTagMapper.java
@@ -6,10 +6,10 @@ import java.util.Map;
 import com.studyveloper.overtheflow.vo.MusicTagVO;
 
 public interface MusicTagMapper {
-	public int addMusicTag(MusicTagVO musicTagVO);
-	public boolean modifyMusicTag(Map<String,Object> map);
+	public Integer addMusicTag(MusicTagVO musicTagVO);
+	public Boolean modifyMusicTag(Map<String,Object> map);
 	public void deleteAllMusicTag();
-	public boolean deleteMusicTag(MusicTagVO musicTagVO);
-	public boolean deleteMusicTagByMusicId(String musicId);
+	public Boolean deleteMusicTag(MusicTagVO musicTagVO);
+	public Boolean deleteMusicTagByMusicId(String musicId);
 	public List<MusicTagVO> searchMusicTagByMusicId(String musicId);
 }
