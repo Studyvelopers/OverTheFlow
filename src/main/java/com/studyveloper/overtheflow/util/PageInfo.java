c75d1a19d61f0c8c6d052851062be518c035a85a
JinGuLee <dlwlsrn94@naver.com>
2019-08-12 14:02:42 +0900

    PageInfo 멤버 변수 변경

--- src/main/java/com/studyveloper/overtheflow/util/PageInfo.java
+++ src/main/java/com/studyveloper/overtheflow/util/PageInfo.java
@@ -6,54 +6,61 @@ import org.springframework.web.util.UriComponentsBuilder;
 public abstract class PageInfo {
 	private Integer currentPageNumber;
 	private Integer perPageCount;
-	private String orderRule;
-	private Integer sort;
+	private String sortionOption;
+	private Boolean ordering;
 	private Integer maxCount;
 	
 	public PageInfo() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public PageInfo(Integer currentPageNumber, Integer perPageCount, String orderRule, Integer sort,
+	
+	public PageInfo(Integer currentPageNumber, Integer perPageCount, String sortionOption, Boolean ordering,
 			Integer maxCount) {
 		super();
 		this.currentPageNumber = currentPageNumber;
 		this.perPageCount = perPageCount;
-		this.orderRule = orderRule;
-		this.sort = sort;
+		this.sortionOption = sortionOption;
+		this.ordering = ordering;
 		this.maxCount = maxCount;
 	}
-	
-	public abstract String makeUrl(String path, Integer pageNumber);
-	
+
 	public Integer getCurrentPageNumber() {
 		return currentPageNumber;
 	}
+
 	public void setCurrentPageNumber(Integer currentPageNumber) {
 		this.currentPageNumber = currentPageNumber;
 	}
+
 	public Integer getPerPageCount() {
 		return perPageCount;
 	}
+
 	public void setPerPageCount(Integer perPageCount) {
 		this.perPageCount = perPageCount;
 	}
-	public String getOrderRule() {
-		return orderRule;
+
+	public String getSortionOption() {
+		return sortionOption;
 	}
-	public void setOrderRule(String orderRule) {
-		this.orderRule = orderRule;
+
+	public void setSortionOption(String sortionOption) {
+		this.sortionOption = sortionOption;
 	}
-	public Integer getSort() {
-		return sort;
+
+	public Boolean getOrdering() {
+		return ordering;
 	}
-	public void setSort(Integer sort) {
-		this.sort = sort;
+
+	public void setOrdering(Boolean ordering) {
+		this.ordering = ordering;
 	}
+
 	public Integer getMaxCount() {
 		return maxCount;
 	}
+
 	public void setMaxCount(Integer maxCount) {
 		this.maxCount = maxCount;
 	}
