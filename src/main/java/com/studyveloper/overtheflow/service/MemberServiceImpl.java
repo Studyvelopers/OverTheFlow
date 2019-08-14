87083919bd41611d01bae74a73d73cf28710f54e
KIM JUNSUNG <naber9065@naver.com>
2019-08-14 20:03:03 +0900

    수정파일 - MemberService.java, MemberServiceImpl.java / 리뷰로 피드백받은 부분인 javadoc작성 위치를 MemberServiceImpl에서 MemberService로 옮겼습니다.

--- src/main/java/com/studyveloper/overtheflow/service/MemberServiceImpl.java
+++ src/main/java/com/studyveloper/overtheflow/service/MemberServiceImpl.java
@@ -26,14 +26,7 @@ public class MemberServiceImpl implements MemberService {
 
 	private static final Logger logger = LoggerFactory.getLogger(MemberServiceImpl.class);
 
-	/**
-	 * 
-	 * @param MemberVO
-	 *            회원 가입을 하기 위한 회원의 정보를 담고있습니다.
-	 * @return MemberVO 회원가입에 성공한 회원의 정보를 반환합니다.
-	 * @throws Exception
-	 *             미정
-	 */
+	
 	public MemberVO register(MemberVO memberVO) throws Exception {
 		boolean idCheck = false;
 		if (memberVO == null) {
@@ -74,14 +67,7 @@ public class MemberServiceImpl implements MemberService {
 		return null;
 	}
 
-	/**
-	 * 
-	 * @param memberId,
-	 *            password 회원 탈퇴를 위한 회원의 식별키와 패스워드입니다.
-	 * @return Boolean 회원 탈퇴를 실패하면 false, 성공하면 true를 반환합니다.
-	 * @throws Exception
-	 *             미정
-	 */
+	
 	public Boolean unRegister(String memberId, String password) throws Exception {
 		boolean result = false;
 		if(memberId == null || memberId.trim().equals("")){
@@ -107,13 +93,7 @@ public class MemberServiceImpl implements MemberService {
 		return result;
 	}
 
-	/**
-	 * 
-	 * @param memberVO, oldPassword 수정할 회원의 정보들을 담은 MemberVO와 수정할 회원의 기존 패스워드를 전달받습니다.
-	 * @return MemberVO 회원정보 수정에 성공하면 수정한 회원의 정보를 반환하고, 실패하면 null을 반환합니다.
-	 * @throws Exception
-	 *             미정
-	 */
+	
 	public MemberVO modifyMember(MemberVO memberVO, String oldPassword) throws Exception {
 		if (memberVO == null) {
 			logger.error("회원정보 수정 실패! - 회원정보를 수정하기위한 MemberVO가 NULL입니다.");
@@ -146,13 +126,7 @@ public class MemberServiceImpl implements MemberService {
 		return null;
 	}
 
-	/**
-	 * 
-	 * @param memberId 회원 정보를 검색하기 위한 회원의 식별키를 전달받습니다.
-	 * @return MemberVO 전달받은 식별키와 일치하는 회원정보가 있다면 검색한 회원의 정보를 반환하고, 실패하면 null을 반환합니다.
-	 * @throws Exception
-	 *             미정
-	 */
+	
 	public MemberVO getMember(String memberId)throws Exception {
 		if(memberId == null || memberId.trim().equals("")){
 			logger.error("회원 검색 실패! - 회원 검색을 위한 memberId가 NULL입니다.");
@@ -171,12 +145,7 @@ public class MemberServiceImpl implements MemberService {
 		return memberVO;
 	}
 
-	/**
-	 * 
-	 * @param PageInfo 요청한 페이지에 대한 정보를 담고있습니다. 
-	 * @return List<MemberVO> 페이지 정보에 부합하는 전체 회원의 정보를 List로 반환합니다. List의 사이즈는 PageInfo의 perpageCount와 같습니다.
-	 * @throws Exception 미정
-	 */
+	
 	public List<MemberVO> getAllMembers(PageInfo pageInfo)throws Exception {
 		if(pageInfo == null){
 			
@@ -208,12 +177,7 @@ public class MemberServiceImpl implements MemberService {
 		return null;
 	}
 
-	/**
-	 * 
-	 * @param SerachInfo 닉네임으로 회원을 검색하기위한 keyword와 조건, page정보들을 담고있습니다.
-	 * @return List<MemberVO> 검색된 회원의 객체를 List로 반환합니다.
-	 * @throws Exception 미정
-	 */
+	
 	public List<MemberVO> getMembersByNickName(SearchInfo searchInfo) throws Exception {
 		if(searchInfo == null){
 			return null;
@@ -253,12 +217,7 @@ public class MemberServiceImpl implements MemberService {
 		return searchMembers;
 	}
 
-	/**
-	 * 
-	 * @param SerachInfo 이메일로 회원을 검색하기위한 keyword와 조건, page정보들을 담고있습니다.
-	 * @return List<MemberVO> 검색된 회원의 객체를 List로 반환합니다.
-	 * @throws Exception 미정
-	 */
+	
 	public List<MemberVO> getMembersByEmail(SearchInfo searchInfo) throws Exception {
 		if(searchInfo == null){
 			return null;
