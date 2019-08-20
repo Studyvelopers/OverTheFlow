2fd3bf86957863213ea19a1c988ed9ae9f184cf4
KIM JUNSUNG <naber9065@naver.com>
2019-08-20 14:31:41 +0900

    수정 파일 - MemberController.java / 회원가입을 위한 메소드 구현및 테스트완료

--- src/main/java/com/studyveloper/overtheflow/controller/MemberController.java
+++ src/main/java/com/studyveloper/overtheflow/controller/MemberController.java
@@ -1,31 +1,43 @@
 package com.studyveloper.overtheflow.controller;
 
 
-import java.util.List;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.studyveloper.overtheflow.mapper.TestMapper;
+import com.studyveloper.overtheflow.bean.MemberBean;
+import com.studyveloper.overtheflow.service.MemberService;
+import com.studyveloper.overtheflow.vo.MemberVO;
 
 @Controller
-public class TestController {
-	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+@RequestMapping(value="/member")
+public class MemberController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
 	@Autowired
-	private TestMapper testMapper;
+	MemberService memberService;
 	
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String home(Locale locale, Model model){
-		logger.info("welcome home!");
-		List<String> result = this.testMapper.search();
-		logger.info(result.size()+"");
-		return "test";
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public String displayRegister(){
+		logger.info("회원가입 화면으로 이동.");
+		return "register";
 	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String register(MemberBean memberBean)throws Exception{
+		logger.info("회원가입 화면으로 이동.");
+		if(memberBean == null ){
+			logger.error("memberBean이 NULL입니다.");
+		}
+		MemberVO memberVO = memberBean.toVO();
+		memberVO = memberService.register(memberVO);
+		return "register";
+	}
+	
+	
 }
