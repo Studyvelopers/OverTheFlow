33f496e6a6e61688d0a5ac2f316e555699df2b0b
DONG-HOON JEON <dhjeon1993@gmail.com>
2019-08-22 15:26:02 +0900

    [추가] PlaylistController - displayCreatePlaylist

--- src/main/java/com/studyveloper/overtheflow/controller/PlaylistController.java
+++ src/main/java/com/studyveloper/overtheflow/controller/PlaylistController.java
@@ -1,10 +1,15 @@
 package com.studyveloper.overtheflow.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/playlist")
 public class PlaylistController {
 	
+	@GetMapping
+	public String displayCreatePlaylist() {
+		return "playlist/create";
+	}
 }
