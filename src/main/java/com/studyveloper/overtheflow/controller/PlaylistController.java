bcad9de954bfcf0d08981eba8e557534d86180af
DONG-HOON JEON <dhjeon1993@gmail.com>
2019-08-22 15:23:18 +0900

    [추가] PlaylistController 생성

---
+++ src/main/java/com/studyveloper/overtheflow/controller/PlaylistController.java
@@ -0,0 +1,10 @@
+package com.studyveloper.overtheflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/playlist")
+public class PlaylistController {
+	
+}
