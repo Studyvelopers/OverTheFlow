e9d288d4d8c984251a1d3cc3e611e25664a37ed5
KIM JUNSUNG <naber9065@naver.com>
2019-08-28 03:37:07 +0900

    수정파일 - MemberController.java / 회원 검색을 위한 메소드 구현 및 테스트 완료

--- src/main/java/com/studyveloper/overtheflow/controller/MemberController.java
+++ src/main/java/com/studyveloper/overtheflow/controller/MemberController.java
@@ -11,14 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.studyveloper.overtheflow.bean.MemberBean;
 import com.studyveloper.overtheflow.service.MemberService;
-import com.studyveloper.overtheflow.util.PageInfo;
 import com.studyveloper.overtheflow.util.SearchInfo;
-import com.studyveloper.overtheflow.util.option.MemberUnit;
 import com.studyveloper.overtheflow.vo.MemberVO;
 
 @Controller
@@ -165,6 +164,7 @@ public class MemberController {
 	
 	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public String displayMembers(HttpSession session, SearchInfo searchInfo, Model model) throws Exception{
+		//전체 조회 관리자용? 권한 조회를 해야하나
 		List<MemberVO> memberVOs = memberService.getAllMembers(searchInfo);
 		List<MemberBean> memberBeans = new ArrayList<>();
 		
@@ -175,5 +175,24 @@ public class MemberController {
 		model.addAttribute("members", memberBeans);
 		return "members";
 	}
-
+	
+	@RequestMapping(value="/search/member/{searchType}", method=RequestMethod.GET)
+	public String searchMembers(HttpSession session,@PathVariable("searchType")String searchType, SearchInfo searchInfo, Model model) throws Exception{
+		logger.info("welcome searchmember");
+		String searchOption = searchInfo.getSearchOption();
+		List<MemberVO> memberVOs = new ArrayList<>();
+		List<MemberBean> members = new ArrayList<>();
+		System.out.println(searchInfo.getKeyword());
+		if(searchType.equals("NICKNAME")){
+			memberVOs = memberService.getMembersByNickName(searchInfo);
+		}else if(searchType.equals("EMAIL")){
+			memberVOs = memberService.getMembersByEmail(searchInfo);
+		}
+		
+		for(int i=0; i<memberVOs.size(); i++){
+			members.add(new MemberBean(memberVOs.get(i)));
+		}
+		model.addAttribute("members", members);
+		return "members";
+	}
 }
