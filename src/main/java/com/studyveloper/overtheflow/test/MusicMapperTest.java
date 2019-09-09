2325693b5f1bffc4e53ba7889b05d0e2afb80d47
JinGuLee <dlwlsrn94@naver.com>
2019-09-09 15:36:41 +0900

    musicAddTest 정상 성공테스트 케이스 추가

--- src/main/java/com/studyveloper/overtheflow/test/MusicMapperTest.java
+++ src/main/java/com/studyveloper/overtheflow/test/MusicMapperTest.java
@@ -1,5 +1,8 @@
 package com.studyveloper.overtheflow.test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,6 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.studyveloper.overtheflow.mapper.MusicMapper;
 import com.studyveloper.overtheflow.util.option.OptionIntent;
 import com.studyveloper.overtheflow.vo.MusicVO;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"file:C:/studyveloper/overtheflow/OverTheFlowWorkSpace/OverTheFlow/src/main/webapp/WEB-INF/spring/root-context.xml"})
@@ -55,9 +59,41 @@ public class MusicMapperTest {
 		this.musicMapper.deleteMusics(builder.build());
 	}
 	
-	//케이스1. 중복 키값이 들어갈 경우
+	//케이스1. 성공적으로 추가가 완료될 경우
+	public void musicAddTest() {
+		MusicVO musicVO = new MusicVO();
+		
+		musicVO.setCategoryId("10");
+		musicVO.setDescription("뮤직 테스트 케이스1");
+		musicVO.setDownloadable(true);
+		musicVO.setId("TESTID0");
+		musicVO.setMemberId("0");
+		musicVO.setMemberNickname("nickname0");
+		musicVO.setPlayCount(0);
+		musicVO.setPlaytime(100);
+		musicVO.setRegisterDate(new Date());
+		
+		List<String> tags = new ArrayList<String>();
+		tags.add("태그1");tags.add("태그2");tags.add("태그3");
+		
+		musicVO.setTags(tags);
+		musicVO.setTitle("테스트케이스1");
+		musicVO.setVisibility(true);
+		
+		int result = -1;
+		try {
+			result = this.musicMapper.addMusic(musicVO);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertThat(result, is(1));
+	}
+	
+	//익셉션 케이스1. 중복 키값이 들어갈 경우
 	@Test(expected=DuplicateKeyException.class)
-	public void musicAddTest() throws Exception {
+	public void musicAddExceptionTest() throws Exception {
 		MusicVO musicVO = new MusicVO();
 	
 			musicVO.setCategoryId("0");
@@ -79,6 +115,8 @@ public class MusicMapperTest {
 			
 			this.musicMapper.addMusic(musicVO);
 	}
+	
+
 	
 	//케이스2. 
 	@Test
