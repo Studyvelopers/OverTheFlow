f24ba636586259823aedfa872834da47e68d1fac
JinGuLee <dlwlsrn94@naver.com>
2019-09-10 11:51:27 +0900

    musicAddUnsatisfiedDataTest 예외 테스트 케이스 추가

--- src/main/java/com/studyveloper/overtheflow/test/MusicMapperTest.java
+++ src/main/java/com/studyveloper/overtheflow/test/MusicMapperTest.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -60,13 +61,14 @@ public class MusicMapperTest {
 	}
 	
 	//케이스1. 성공적으로 추가가 완료될 경우
+	@Test
 	public void musicAddTest() {
 		MusicVO musicVO = new MusicVO();
 		
-		musicVO.setCategoryId("10");
+		musicVO.setCategoryId("0");
 		musicVO.setDescription("뮤직 테스트 케이스1");
 		musicVO.setDownloadable(true);
-		musicVO.setId("TESTID0");
+		musicVO.setId("TESTID10");
 		musicVO.setMemberId("0");
 		musicVO.setMemberNickname("nickname0");
 		musicVO.setPlayCount(0);
@@ -93,7 +95,7 @@ public class MusicMapperTest {
 	
 	//익셉션 케이스1. 중복 키값이 들어갈 경우
 	@Test(expected=DuplicateKeyException.class)
-	public void musicAddExceptionTest() throws Exception {
+	public void musicAddDuplicateKeyTest() throws Exception {
 		MusicVO musicVO = new MusicVO();
 	
 			musicVO.setCategoryId("0");
@@ -117,10 +119,30 @@ public class MusicMapperTest {
 	}
 	
 
-	
-	//케이스2. 
-	@Test
-	public void UnmatchedNicknameAddTest(){
+	//DuplicateKeyException이 DataIntegrityViolationException을 상속받음
+	//익셉션 케이스2. 필요한 데이터들이 다 제공되지 않았을 경우
+	@Test(expected=DataIntegrityViolationException.class)
+	public void musicAddUnsatisfiedDataTest() throws Exception{
+		MusicVO musicVO = new MusicVO();
 		
+		musicVO.setCategoryId("0");
+		musicVO.setDescription("뮤직 테스트 케이스1");
+		musicVO.setDownloadable(true);
+		musicVO.setId("TESTID10");
+		//musicVO.setMemberId("0");
+		musicVO.setMemberNickname("nickname0");
+		musicVO.setPlayCount(0);
+		musicVO.setPlaytime(100);
+		//날짜는 default값이 걸려있어서 상관없음
+		//musicVO.setRegisterDate(new Date());
+		
+		List<String> tags = new ArrayList<String>();
+		tags.add("태그1");tags.add("태그2");tags.add("태그3");
+		
+		musicVO.setTags(tags);
+		musicVO.setTitle("테스트케이스1");
+		musicVO.setVisibility(true);
+		
+		this.musicMapper.addMusic(musicVO);
 	}
 }
